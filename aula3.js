9bf4d29c720afadafb902cdce8c591b6eac8e426
Gabriel Santos <74970037+Hebreuson@users.noreply.github.com>
2023-12-06 20:37:21 -0300

    javaScript
    apredemos o LET,CONST E O VAR (diferença entre eles)

--- aula3.js
+++ aula3.js
@@ -1,3 +1,50 @@
-const investimentoMensal = [1000, 2000, 3000, 1000, 5000];
+// const investimentoMensal = [1000, 2000, 3000, 1000, 5000];
 
-console.log(` O valor investido no mês de janeiro foi: `)
+// console.log(` O valor investido no mês de janeiro foi: `)
+
+//         var nome = prompt ('Qual é o seu nome ?')
+//         alert('Satisfação em te conhecer ' + nome);
+//         var cidade = prompt ('Qual é a sua cidade ?');
+//         alert('Partiu sequestro haha em ' + cidade);
+
+//         var idade = "eu tenho 18 anos"; /*  qunado tem numero, NÃO COLOCA ENTRE PARENTEZES, MAS SIM ENTRE ASPAS*/ 
+
+//         console.log('idade')
+
+
+// var idade = 'eu tenho 18 anos';
+// console.log(idade);
+ 
+
+//    VAR (variavel)  
+//    CONST () serve igual ao VAR
+//    LET ()  serve igual ao VAR
+   
+
+
+// quando for declarar uma um numero, esquece aspas simples ou duplas
+
+//const nome = ('Igor');
+//nome = ('Renato');
+
+//console.log(nome);
+
+
+// (const) usado para declarar uma variavel de valor fixo, ou seja, que nuca irá ser alterado
+
+// (let) é usado para declarar uma variavel NÃO fixa, podendo ser alterada futuramente.
+
+const mensagemTela = ('Este texto é um exemplo para podermos praticar os exercicios'); 
+
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+
+// quando se escreve CLEAR no terminal de execução do JavaScript, serve para limpar a tela
+
+let messagemExemplo = ('essa mensagem e um teste');
+console.log(mensagemExemplo);
+
