7781937772667a4221f3350cadb0a8ebd4e5ec86
Gabriel Santos <74970037+Hebreuson@users.noreply.github.com>
2023-12-07 21:01:43 -0300

    JavaScript

--- aula3.js
+++ aula3.js
@@ -1,20 +1,21 @@
 // // const investimentoMensal = [1000, 2000, 3000, 1000, 5000];
-
 // // console.log(` O valor investido no mês de janeiro foi: `)
 
 // //         var nome = prompt ('Qual é o seu nome ?')
 // //         alert('Satisfação em te conhecer ' + nome);
 // //         var cidade = prompt ('Qual é a sua cidade ?');
 // //         alert('Partiu sequestro haha em ' + cidade);
-
 // //         var idade = "eu tenho 18 anos"; /*  qunado tem numero, NÃO COLOCA ENTRE PARENTEZES, MAS SIM ENTRE ASPAS*/ 
-
 // //         console.log('idade')
+
+
 
 
 // // var idade = 'eu tenho 18 anos';
 // // console.log(idade);
  
+
+
 
 // //    VAR (variavel)  
 // //    CONST () serve igual ao VAR
@@ -22,45 +23,88 @@
    
 
 
-// // quando for declarar uma um numero, esquece aspas simples ou duplas
 
+// // quando for declarar uma um numero, esquece aspas simples ou duplas
 // //const nome = ('Igor');
 // //nome = ('Renato');
-
 // //console.log(nome);
 
 
+
+
 // // (const) usado para declarar uma variavel de valor fixo, ou seja, que nuca irá ser alterado
-
 // // (let) é usado para declarar uma variavel NÃO fixa, podendo ser alterada futuramente.
-
 // const mensagemTela = ('Este texto é um exemplo para podermos praticar os exercicios'); 
 
+
+
 // console.log(mensagemTela);
 // console.log(mensagemTela);
 // console.log(mensagemTela);
 // console.log(mensagemTela);
 // console.log(mensagemTela);
 // console.log(mensagemTela);
+
+
 
 // // quando se escreve CLEAR no terminal de execução do JavaScript, serve para limpar a tela
-
 // let messagemExemplo = ('essa mensagem e um teste');
 // console.log(mensagemExemplo);
 
 
-// operadores JavaScript
 
+
+// operadores JavaScript
 // (+) = Adição
 // (-) = Subtração
 // (/) = Divisão
 // (*) = Multiplicação
 
-const resultadoSoma = 25/35;
-console.log(resultadoSoma);
 
-const texto1 = 'Olá meu nome é Davi'
-const texto2 = ' Eu tenho 20 anos'
-const textoFinal = texto1 + texto2 // paramos por aqu, se usarmos sinal de (-) vai dar merda kkk 
 
-console.log(textoFinal)
+
+
+// const resultadoSoma = 25-35;
+// console.log(resultadoSoma);
+
+
+
+
+
+// const texto1 = 'Olá meu nome é Davi'
+// const texto2 = ' Eu tenho 20 anos'
+// const textoFinal = texto1 + texto2 // paramos por aqu, se usarmos sinal de (-) vai dar merda kkk 
+// console.log(textoFinal)
+
+
+
+
+
+// // STRING é um texto corrido, smp vai esta dentro de uma variavel !! 
+// const texto1 ='Aqui temos uma STRING normal'
+// const texto2 ="Ja esta, apesar das aspas duplas ao inves das simples tambem é uma string normal"
+// const numeroRegistrar = 31472;
+// const stringTemplete = ` O numero que quais registrar foi o ${numeroRegistrar}`;
+// console.log(stringTemplete);
+
+
+
+
+
+
+
+// [] isso é denominado como ARRAY
+// {} isso usamos para stringTemples !!
+// () dominado como string "normal" texto corrido
+
+
+
+// const arreyNumber = [1, 2, 3, 4, 5];
+// console.log(arreyNumber);
+// console.log(` O valor do elemento 0 da minha lista é ${arreyNumber[0]} `)
+
+
+
+// const arreyNumber = [10, 20, 30, 40, 50,];
+// console.log(arreyNumber);
+// console.log(` O valor do elemento 0 da minha lista é ${arreyNumber[0]} `);
